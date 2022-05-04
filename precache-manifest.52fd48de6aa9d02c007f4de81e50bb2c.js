eaedfcdd77d62f955057833959ebc9b0e0fd2958
BhavinGhevariya <BhavinGhevariya@users.noreply.github.com>
2022-05-04 10:39:52 +0000

    deploy: f7acd940ade2fdbc39d4225f46d28b8805bfd53f

---
+++ precache-manifest.52fd48de6aa9d02c007f4de81e50bb2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3adf6bd4bf0ec553977a46f176feb2cd",
+    "url": "https://BhavinGhevariya.github.io/task-management-frontend\n/index.html"
+  },
+  {
+    "revision": "88a937fa14c42fd2f69a",
+    "url": "https://BhavinGhevariya.github.io/task-management-frontend\n/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "09c837cd62fca298ac9a",
+    "url": "https://BhavinGhevariya.github.io/task-management-frontend\n/static/js/2.65ac4a1a.chunk.js"
+  },
+  {
+    "revision": "88a937fa14c42fd2f69a",
+    "url": "https://BhavinGhevariya.github.io/task-management-frontend\n/static/js/main.2dd601c5.chunk.js"
+  },
+  {
+    "revision": "336937cfc2a54c50102f",
+    "url": "https://BhavinGhevariya.github.io/task-management-frontend\n/static/js/runtime~main.b77bd6c8.js"
+  }
+]);
